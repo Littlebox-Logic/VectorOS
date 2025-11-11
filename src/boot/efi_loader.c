e3dbd99e7363173171adbb9df6e32b971450376b
Littlebox-Logic <sunjunpeng2007@126.com>
2025-11-11 22:51:20 +0800

    Basic EFI info display.

--- src/boot/efi_loader.c
+++ src/boot/efi_loader.c
@@ -7,10 +7,17 @@
 
 #include <efi.h>
 #include <efilib.h>
+#include <stdbool.h>
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
 {
-	Print(L"Hello world.");
-	while (1);
+	InitializeLib(image_handle, system_table);
+	system_table -> ConOut -> ClearScreen(system_table -> ConOut);
+	Print(L"Hello world. - Vector OS 0.1.0.0 (Build 0)\n\n");
+	Print(L"System Info:\n");
+	Print(L"\tFirmware Vendor   : %s\n", system_table -> FirmwareVendor);
+	Print(L"\tFirmware Revision : %d\n", system_table -> FirmwareRevision);
+	Print(L"\tEFI Specification : %d.%02d\n", system_table -> Hdr.Revision >> 16, system_table -> Hdr.Revision & 0xFFFF);
+	while (true);
 	return EFI_SUCCESS;
 }
